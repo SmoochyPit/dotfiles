e24a2b401a8b283ccbecfc035af21c670a07e12d
SmoochyPit <smoochypit@gmail.com>
2024-11-17 12:43:41 -0700

    Enable vim binds in fish, add cuda to fish path

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,9 @@ if status is-interactive
     source ~/.config/fish/aliases.fish
     source ~/.config/fish/env.fish
 
+    fish_vi_key_bindings
     fish_add_path /home/smooch/.cargo/bin
+    fish_add_path /opt/cuda/bin
 end
 
 set -U fish_greeting
